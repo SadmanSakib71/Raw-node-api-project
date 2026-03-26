68a274db0e31ccd07f301e0459ee5c1b8de33e5a
Sadman Sakib <155054940+SadmanSakib71@users.noreply.github.com>
2026-03-26 07:05:32 +0600

    server runing successfully

---
+++ index.js
@@ -0,0 +1,30 @@
+//dependencies
+const http = require("node:http");
+
+//app object - module scaffolding
+
+const app = {};
+
+//configuration
+
+app.config = {
+  port: 3000,
+};
+
+//create server
+
+app.createServer = () => {
+  const server = http.createServer(app.handleReqRes);
+  server.listen(app.config.port, () => {
+    console.log(`server run on ${app.config.port} port`);
+  });
+};
+
+//handle Request Response
+
+app.handleReqRes = (req, res) => {
+  //response handle
+  res.end("Hello world");
+};
+
+app.createServer();
