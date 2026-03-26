d256aef8e224fff7207a9f01fd5345ab67fd6365
Sadman Sakib <155054940+SadmanSakib71@users.noreply.github.com>
2026-03-26 08:44:03 +0600

    parsing request

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 //dependencies
 const http = require("node:http");
+const url = require("node:url");
+const { StringDecoder } = require("node:string_decoder");
 
 //app object - module scaffolding
 
@@ -23,8 +25,34 @@ app.createServer = () => {
 //handle Request Response
 
 app.handleReqRes = (req, res) => {
-  //response handle
-  res.end("Hello world bye hello");
+  //request handle
+
+  //get the url and parse it
+  const parseUrl = url.parse(req.url, true);
+  const path = parseUrl.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$/g, "");
+  //get the method
+  const method = req.method.toLowerCase();
+  //get query string
+  const queryStringObject = parseUrl.query;
+  //get header
+  const headerObject = req.headers;
+
+  //get data from body
+  const decoder = new StringDecoder("utf-8");
+  let realData = "";
+
+  req.on("data", (buffer) => {
+    realData += decoder.write(buffer);
+  });
+
+  req.on("end", () => {
+    realData += decoder.end();
+    console.log(realData);
+
+    //response handle
+    res.end("Hello world bye hello");
+  });
 };
 
 app.createServer();
