6642f3243bca2b298db77c4ca27cc00ee5d85c2e
Sadman Sakib <155054940+SadmanSakib71@users.noreply.github.com>
2026-03-29 05:02:16 +0600

    setUp user route

--- handlers/routeHandlers/userHandler.js
+++ handlers/routeHandlers/userHandler.js
@@ -2,9 +2,21 @@
 const handler = {};
 
 handler.userHandler = (requestProperties, callBack) => {
-  callBack(200, {
-    title: "this is user handler",
-  });
+  const acceptMethods = ["get", "post", "put", "delete"];
+  if (acceptMethods.indexOf(requestProperties.method) > -1) {
+    handler_users[requestProperties.method](requestProperties, callBack);
+  } else {
+    callBack(405);
+  }
 };
+
+const handler_users = {};
+
+handler_users.post = (requestProperties, callBack) => {};
+handler_users.get = (requestProperties, callBack) => {
+  callBack(200);
+};
+handler_users.put = (requestProperties, callBack) => {};
+handler_users.delete = (requestProperties, callBack) => {};
 
 module.exports = handler;
