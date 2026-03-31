b04593510db5dd2e8872772d33c0024d7dfe9d3a
Sadman Sakib <155054940+SadmanSakib71@users.noreply.github.com>
2026-03-31 08:53:57 +0600

    validate inputs

--- handlers/routeHandlers/checkHandler.js
+++ handlers/routeHandlers/checkHandler.js
@@ -2,6 +2,7 @@
 const data = require("../../lib/data");
 const { hashing, parseJSON } = require("../../helpers/utilities");
 const tokenHandler = require("./tokenHandler");
+const { ary } = require("lodash");
 
 //module scaffolding
 const handler = {};
@@ -18,7 +19,40 @@ handler.checkHandler = (requestProperties, callBack) => {
 const handler_check = {};
 
 //post method
-handler_check.post = (requestProperties, callBack) => {};
+handler_check.post = (requestProperties, callBack) => {
+  //validate inputs
+  let protocol =
+    typeof requestProperties.body.protocol === "string" &&
+    ["http", "https"].indexOf(requestProperties.body.protocol) > -1
+      ? requestProperties.body.protocol
+      : false;
+
+  let url =
+    typeof requestProperties.body.url === "string" &&
+    requestProperties.body.url.trim().length > 0
+      ? requestProperties.body.url
+      : false;
+
+  let method =
+    typeof requestProperties.body.method === "string" &&
+    ["get", "post", "put", "delete"].indexOf(requestProperties.body.method) > -1
+      ? requestProperties.body.method
+      : false;
+
+  let successCodes =
+    typeof requestProperties.body.successCodes === "object" &&
+    requestProperties.body.successCodes instanceof Array
+      ? requestProperties.body.successCodes
+      : false;
+
+  let timeOutSeconds =
+    typeof requestProperties.body.timeOutSeconds === "number" &&
+    requestProperties.body.timeOutSeconds % 1 === 0 &&
+    requestProperties.body.timeOutSeconds >= 1 &&
+    requestProperties.body.timeOutSeconds <= 5
+      ? requestProperties.body.timeOutSeconds
+      : false;
+};
 
 //get method
 handler_check.get = (requestProperties, callBack) => {};
