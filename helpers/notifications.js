599ea95819faa6060a306c4c19dec85e9239d702
Sadman Sakib <155054940+SadmanSakib71@users.noreply.github.com>
2026-04-01 07:19:30 +0600

    create notification file for twilio

---
+++ helpers/notifications.js
@@ -0,0 +1,10 @@
+//dependencies
+const https = require("node:https");
+
+//module object-scaffolding
+const notification = {};
+
+//send sms to user using twilio api
+notification.sendTwilioSms = (phone, sms, callBack) => {};
+
+module.exports = notification;
